acb7f124b1e4502fcd8f4b3654b35b747fbe1294
surbhi <surbhisaga337865@gmail.com>
2022-06-01 11:44:34 +0530

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <nav className="navbar navbar-expand-lg bg-dark">
+          <Link className="navbar-brand" to="#">
+            Navbar
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="#">tutorial</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="#">Add</Link>
+            </li>
+          </div>
+        </nav>
+        {/* <div className="container mt-3">
+          <Switch>
+            <Route exact path="#" />
+          </Switch>
+        </div> */}
+      </Router>
+    );
+  }
 }
 
 export default App;
