b112bb12c0b06c54fb2fccfd7804235066b89251
surbhi <surbhisaga337865@gmail.com>
2022-06-01 15:30:06 +0530

    crud app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import TutorialsList from "./components/TutorialsList";
+import AddTutorial from "./components/AddTutorial";
+import Tutorial from "./components/Tutorial";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <nav className="navbar navbar-expand-lg bg-dark">
-          <Link className="navbar-brand" to="#">
-            Navbar
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/"} className="navbar-brand">
+            bezKoder
           </Link>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to="#">tutorial</Link>
+              <Link to={"/tutorials"} className="nav-link">
+                Tutorials
+              </Link>
             </li>
             <li className="nav-item">
-              <Link to="#">Add</Link>
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
             </li>
           </div>
         </nav>
-        {/* <div className="container mt-3">
-          <Switch>
-            <Route exact path="#" />
-          </Switch>
-        </div> */}
+        <div className="container mt-3">
+          <Routes>
+            <Route exact path="/tutorials" element={<TutorialsList/>} />
+            <Route exact path="/add" element={<AddTutorial />} />
+            <Route path="/tutorials/:id" element={<Tutorial />} />
+          </Routes>
+        </div>
       </Router>
     );
   }
 }
-
 export default App;
